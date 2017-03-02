a195db5a45ba0263d3884227781a9d65192b0163
gitConfig01 <hsf_swift_01@sina.com>
2017-03-02 09:58:31 +0800

    添加瞌睡泡泡效果的View
    添加瞌睡泡泡效果的View

--- hsfCategoryDemo/hsfCategoryDemo/FirstVC.m
+++ hsfCategoryDemo/hsfCategoryDemo/FirstVC.m
@@ -9,8 +9,11 @@
 #import "FirstVC.h"
 
 #import "SecondVC.h"
+#import "UIView+YGPulseView.h"
 
 @interface FirstVC ()
+@property (weak, nonatomic) IBOutlet UIView *testView;
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
@@ -18,7 +21,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.testView startPulseWithColor:[UIColor redColor] animation:YGPulseViewAnimationTypeRadarPulsing];
     
+    
+    // 换行符：\n 不起作用
+    self.testLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    self.testLabel.text = @"但是疯狂减肥后开始点击佛教佛佛教啊结束哦\n大煞风景福建省累计罚哦时间佛\n的身份告诉对方iiii附近开了多少积分大煞风景哦";
 }
 - (IBAction)pushToSecondVC:(UIButton *)sender {
     SecondVC *vc = [[SecondVC alloc]init];
